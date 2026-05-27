dd9ac97d8485f5b38dd112abaddafcacd0970f42
Ramiro Veloso <61808653+ramiroveloso@users.noreply.github.com>
2026-05-27 14:46:49 -0300

    agregó url de pruebas

--- gol_ahora_back/settings.py
+++ gol_ahora_back/settings.py
@@ -163,15 +163,17 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Todos los frontends permitidos para comunicarse con la API
 CORS_ALLOWED_ORIGINS = [
-    "https://gol-ahora-frontend.vercel.app",
-    "https://gol-ahora-frontend-f4kvtosqj-ramiro-veloso-s-projects.vercel.app",  # Tu URL de pruebas actual según la consola
+    "https://gol-ahora-frontend.vercel.app", "https://gol-ahora-backend-pruebas.onrender.com",
+ "https://gol-ahora-backend-pruebas.onrender.com",
+"https://gol-ahora-ramiro-pruebas.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://gol-ahora-frontend.vercel.app", "https://gol-ahora-backend-1.onrender.com",
-    "http://localhost:5173",
+"https://gol-ahora-backend-pruebas.onrender.com", 
+"http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 # =========================================================================
