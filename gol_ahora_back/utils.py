4094852f7b4cb78ecaebb1c761e7fd92c0f684f0
Ramiro Veloso <61808653+ramiroveloso@users.noreply.github.com>
2026-05-27 12:16:45 -0300

    cambio en manejo de objeto fecha para ver reservas en caliente recién hechas

--- gol_ahora_back/utils.py
+++ gol_ahora_back/utils.py
@@ -1,16 +1,23 @@
-from datetime import datetime, timezone
+from datetime import datetime, time, timezone
 from rest_framework import serializers
+from django.utils import timezone as django_timezone
 
 
 class EpochMillisecondsField(serializers.Field):
     """
-    Serializa DateTimeField de Django como epoch milliseconds (entero).
+    Serializa DateTimeField y DateField de Django como epoch milliseconds (entero).
     Deserializa epoch milliseconds (entero) a un objeto datetime de Django (UTC).
     """
 
     def to_representation(self, value):
         if not value:
             return None
+        
+        # Si es un objeto date puro (no tiene el método timestamp), lo convertimos
+        # a datetime combinándolo con hora 00:00 y asignándole la zona horaria UTC
+        if not hasattr(value, 'timestamp') and hasattr(value, 'year'):
+            value = django_timezone.make_aware(datetime.combine(value, time.min), django_timezone.utc)
+            
         return int(value.timestamp() * 1000)
 
     def to_internal_value(self, data):
