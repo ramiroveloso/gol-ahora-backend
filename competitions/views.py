e9ffcc8f05859f0d0407b5d1432db15389fd85b5
Ramiro Veloso <61808653+ramiroveloso@users.noreply.github.com>
2026-05-30 15:00:09 -0300

    Actualizar views.py para mostrar competencias en el front

--- competitions/views.py
+++ competitions/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+# pyrefly: ignore [missing-import]
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Competencia
+from .serializers import CompetenciaSerializer
+
+class CompetenciaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que expone las competencias.
+    Filtra para mostrar solo en las que el usuario logueado participa.
+    """
+    serializer_class = CompetenciaSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        
+        # Si es un administrador, permitimos ver todas para gestionar
+        if user.is_staff or user.rol == 'ADMIN':
+            return Competencia.objects.all()
+            
+        # Para los clientes, filtramos las competencias donde su usuario está en la lista de participantes
+        return Competencia.objects.filter(participantes=user, activa=True)
