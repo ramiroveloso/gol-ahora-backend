48ad199ee7f2fb5d9fb085cd72cf715b22d6e079
Ramiro Veloso <61808653+ramiroveloso@users.noreply.github.com>
2026-05-30 10:14:55 -0300

    texto control de canchas

--- fields/apps.py
+++ fields/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class FieldsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fields'
+
+    verbose_name = 'Control de Canchas'    # <-- Texto personalizado
