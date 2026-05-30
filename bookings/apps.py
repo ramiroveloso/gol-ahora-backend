e9ae6135abaed974a704eda965b8d21c8e91aa88
Ramiro Veloso <61808653+ramiroveloso@users.noreply.github.com>
2026-05-30 10:13:56 -0300

    cambio de texto reservas panel super usuario

--- bookings/apps.py
+++ bookings/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class BookingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookings'
+
+verbose_name = 'Gestión de Reservas' 
